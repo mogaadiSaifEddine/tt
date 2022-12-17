e2456fdffba4e12146fe09b8bbb8ce8753c58ca2
saifmogaadi12 <mogaadisaif7@gmail.com>
2022-12-17 13:39:13 +0100

    advisor feature impl

--- src/main/java/com/example/backendproject/entities/Chapter.java
+++ src/main/java/com/example/backendproject/entities/Chapter.java
@@ -22,6 +22,12 @@ public class Chapter {
 
     private Trimestre trimestre;
 
+
+
+
+    @Column(columnDefinition = "boolean default true")
+     private  Boolean isValid ;
+
     @JsonIgnore
     @ManyToOne
     private User teacher ;
