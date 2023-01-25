d638f72cadf48eec9ae3471b08c41434e1f86fcd
saifmogaadi12 <mogaadisaif7@gmail.com>
2023-01-25 09:09:44 +0100

    some chnages  and fixes here

--- src/main/java/com/example/backendproject/repos/ExercBlockRepository.java
+++ src/main/java/com/example/backendproject/repos/ExercBlockRepository.java
@@ -1,8 +1,10 @@
 package com.example.backendproject.repos;
 
 import com.example.backendproject.entities.Exercice_Block;
-import com.example.backendproject.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExercBlockRepository extends JpaRepository<Exercice_Block, Long> {
+
+
+//    findyByUserExercice(Long user_id , Long exercice_id);
 }
