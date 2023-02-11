dafa9cc4723c7a96191c34d97a3712a69135ada4
saifmogaadi12 <mogaadisaif7@gmail.com>
2023-02-11 21:00:55 +0100

    changing chapter files to url

--- src/main/java/com/example/backendproject/entities/Chapter.java
+++ src/main/java/com/example/backendproject/entities/Chapter.java
@@ -3,6 +3,7 @@ package com.example.backendproject.entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -16,65 +17,43 @@ public class Chapter {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long chapter_id;
+
     private String name;
-    private  String url ;
+
+    private String url;
+
     private ChapterType chapterType;
 
     private Trimestre trimestre;
 
-
-
-
     @Column(columnDefinition = "boolean default true")
-     private  Boolean isValid ;
+    private Boolean isValid;
 
     @JsonIgnore
     @ManyToOne
-    private User teacher ;
+    private User teacher;
 
+    private String catre_conceptuelle;
 
-    @OneToOne(cascade = {CascadeType.REMOVE})
-    @JoinColumn(name = "catre_conceptuelle_carte_id")
-    private    Catre_Conceptuelle catre_conceptuelle;
-
-
-    @OneToOne()
-    @JoinColumn(name = "course")
-    private    Course course;
-
-
-    @OneToOne()
-    @JoinColumn(name = "coursePR")
-    private    CoursePR coursePR;
-
-
-
-
+    private String coursePR;
+    private String coursePRstring;
 
     @ManyToOne
-   @JsonIgnoreProperties("chaptersList")
-   private Group group;
+    @JsonIgnoreProperties("chaptersList")
+    private Group group;
 
-
-
-
-
-    @OneToMany(mappedBy = "parent" ,  fetch = FetchType.EAGER,   orphanRemoval=true)
+    @OneToMany(mappedBy = "parent", fetch = FetchType.EAGER, orphanRemoval = true)
     @ToString.Exclude
     private List<Chapter> children;
 
-    @ManyToOne( )
+    @ManyToOne()
     @ToString.Exclude
-    @JsonIgnoreProperties({"children" , "group"})
+    @JsonIgnoreProperties({"children", "group"})
     private Chapter parent;
 
-    @OneToMany  (  mappedBy = "chapter", fetch = FetchType.EAGER, cascade = CascadeType.PERSIST, orphanRemoval = true)
+    @OneToMany(mappedBy = "chapter", fetch = FetchType.EAGER, cascade = CascadeType.PERSIST, orphanRemoval = true)
     @JsonIgnore
     @ToString.Exclude
     private List<ExerciceSerie> exerciceSeries;
-
-
-
-
 
 }
